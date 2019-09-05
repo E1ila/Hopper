181595fa368f9430e5678f9c46a6ce9bbd9db6a1
ko0f <E1ila@users.noreply.github.com>
2019-09-05 09:30:04 +0300

    send messages on addon channel

--- Hopper.lua
+++ Hopper.lua
@@ -4,25 +4,30 @@ local function print(text)
     DEFAULT_CHAT_FRAME:AddMessage(text)
 end
 
+local function emptyIfNil(text) 
+	if text == nil then return "" end 
+	return text
+end 
+
 ------------------------------------------------
 
 function Hopper_OnLoad(self)
-	realmName = GetRealmName()
-	faction = UnitFactionGroup("player")
-	playerName = UnitName("player")
+	gRealmName = GetRealmName()
+	gFaction = UnitFactionGroup("player")
+	gPlayerName = UnitName("player")
 
 	SLASH_Hopper1 = "/hop"
     SlashCmdList["Hopper"] = Hopper_Main
 
-	print("|cFFFF8080 Hopper |rLoaded, write |cFF00FF00/ahdump help|r for options")
+	print("|cFFFF8080 Hopper |rLoaded, write |cFF00FF00/hop help|r for options")
 
-	-- this:RegisterEvent("PLAYER_TARGET_CHANGED")
-	self:RegisterEvent("AUCTION_ITEM_LIST_UPDATE")
+	self:RegisterEvent("CHAT_MSG_ADDON")
 end 
 
-function Hopper_OnEvent(event, arg1)
+function Hopper_OnEvent(event, arg1, arg2, arg3, arg4)
 	-- print("|cFFFF8080 Hopper |r  Hopper_OnEvent  "..event)
-	if (event == "AUCTION_ITEM_LIST_UPDATE") then
+	if (event == "CHAT_MSG_ADDON") then
+		print("|cFFFF8080 Hopper |r Incoming Message: ["..emptyIfNil(arg1).."] ["..emptyIfNil(arg2).."] ["..emptyIfNil(arg3).."] ["..emptyIfNil(arg4).."]")
 	end
 end
 
@@ -32,6 +37,8 @@ end
 function Hopper_Main(msg) 
 	local _, _, cmd, arg1 = string.find(string.upper(msg), "([%w]+)%s*(.*)$");
 	if not cmd then
+	elseif  "S" == cmd or "SEND" == cmd then
+		SendAddonMessage("manual", arg1, "PARTY")
 	elseif  "R" == cmd or "RESET" == cmd then
     elseif  "H" == cmd or "HELP" == cmd then
         print("|cFFFF8080 Hopper |rCommands: ")
